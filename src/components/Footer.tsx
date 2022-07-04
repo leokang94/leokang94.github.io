f505f64fbb8cb64c28bd02eb5f9677ae6a256471
Taehun Kang <acccdang8659@gmail.com>
2022-07-05 03:11:22 +0900

    imp: Footer 꾸미기

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faLinkedin,
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-gray-100 py-6 flex flex-col items-center gap-2">
+      <div className="flex justify-center items-center gap-4">
+        <a href="mailto:acccdang8659@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </a>
+        <a href="https://github.com/acccdang" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kang-taehun-838325149/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/acccdang/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+      </div>
+    </footer>
+  );
 }
