8136196e96118b3bb65d085a17b53a5affd63b13
Taehun Kang <acccdang8659@gmail.com>
2022-07-06 03:14:50 +0900

    config 수정

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const TsconfigPathsPlugins = require('tsconfig-paths-webpack-plugin');
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? homepage : '',
+  images: {
+    loader: 'imgix',
+    path:
+      process.env.NODE_ENV === 'production'
+        ? homepage
+        : 'http://localhost:3000',
+  },
   webpack: (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugins());
 
