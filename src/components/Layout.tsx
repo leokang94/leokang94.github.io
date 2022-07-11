c8f8d92b5f9d94bd4c9c4045653660046147546b
Taehun Kang <acccdang8659@gmail.com>
2022-07-12 01:42:26 +0900

    imp: footer 하단 고정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,9 @@ interface LayoutProps {
 }
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-auto min-h-[100vh]">
       <Navbar />
-      <main className="px-4 md:w-[48rem] md:max-w-3xl md:m-auto">
+      <main className="flex-1 px-4 md:w-[48rem] md:max-w-3xl md:m-auto">
         {children}
       </main>
       <Footer />
