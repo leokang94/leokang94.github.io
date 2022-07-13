3c4803ec7963a318f82f0c573944ba2ff93d09f3
Taehun Kang <acccdang8659@gmail.com>
2022-07-14 01:21:53 +0900

    imp: PostList에서 slug 대신 title을 출력하도록 변경

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -7,10 +7,10 @@ interface PostListProps {
 export default function PostList({ posts }: PostListProps) {
   return (
     <ul className="list-disc list-inside">
-      {posts.map(({ slug }) => (
+      {posts.map(({ slug, frontmatter }) => (
         <Link key={slug} href={{ pathname: '/blog/[slug]', query: { slug } }}>
           <li className="cursor-pointer hover:text-blue-400 transition-colors">
-            {slug}
+            {frontmatter.title}
           </li>
         </Link>
       ))}
