716e5c26485333244e8d270f487d869cf41a09ff
Taehun Kang <acccdang8659@gmail.com>
2022-09-12 14:57:28 +0900

    imp: Profile 구현
    mock 이미지 -> animoji.webp로 변경함.
    이름에 대해 다양하게 표현하고 싶어, react-text-transition을 이용해 구현

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
 import { NextSeo } from 'next-seo';
+
 import Profile from '#components/Profile';
 
 export default function About() {
   return (
     <>
       <NextSeo title="About" />
+      <h1 className="text-5xl mt-10">About</h1>
       <Profile />
     </>
   );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,23 +1,68 @@
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
+
+const NAMES = ['Leo', 'Taehun', '강태훈'];
+const PREFIXES = ['반가워요,', "Hi, I'm"];
+const SUFFIXES = ['입니다!', '!'];
 
 export default function Profile() {
+  const [nameIndex, setNameIndex] = useState(0);
+  const [affixIndex, setAffixIndex] = useState(0);
+
+  useEffect(() => {
+    const nameIntervalId = setInterval(
+      () => setNameIndex((index) => index + 1),
+      3000,
+    );
+    const wrapperIntervalId = setInterval(
+      () => setAffixIndex((index) => index + 1),
+      6000,
+    );
+
+    return () => {
+      clearInterval(nameIntervalId);
+      clearInterval(wrapperIntervalId);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col sm:flex-row items-center gap-6 w-full p-4">
-      <div className="h-[4.5rem] w-[4.5rem] relative flex-shrink-0">
+      <div className="h-[8rem] w-[8rem] relative flex-shrink-0">
         <Image
           className="rounded-full"
-          src="/profile_test.png"
+          src="/profile.webp"
           alt="me"
           layout="fill"
           objectFit="cover"
+          priority
         />
       </div>
 
-      <div className="flex flex-col justify-center gap-1">
-        <strong className="text-2xl">강태훈 (Leo)</strong>
-        <p>
-          프론트엔드에 관심이 많은 개발자입니다. 온라인에서는 악당이라는
-          닉네임으로 활동하고 있습니다.
+      <div className="flex flex-col justify-center gap-1 w-full">
+        <div className="flex gap-[0.4rem] text-2xl mb-3">
+          <TextTransition springConfig={presets.gentle} direction="down" inline>
+            {PREFIXES[affixIndex % PREFIXES.length]}
+          </TextTransition>
+          <strong>
+            <TextTransition
+              springConfig={presets.gentle}
+              direction="down"
+              inline
+            >
+              {NAMES[nameIndex % NAMES.length]}
+            </TextTransition>
+          </strong>
+          <TextTransition springConfig={presets.gentle} direction="down" inline>
+            {SUFFIXES[affixIndex % SUFFIXES.length]}
+          </TextTransition>
+        </div>
+        <p className="text-gray-500 font-light">
+          정확히 아는것을 중요하게 생각합니다.
+          <p>
+            다양한 기술에 관심이 있지만, 현재는 프론트엔드 개발을 주력으로 하고
+            있습니다.
+          </p>
         </p>
       </div>
     </div>
