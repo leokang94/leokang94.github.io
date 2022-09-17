8e8053030a627c8ac9e9b5b3f8ea0dea52db89bb
Taehun Kang <acccdang8659@gmail.com>
2022-09-18 01:14:11 +0900

    imp: toc 개선
    - 스크롤 도중엔 intersection observer가 동작하지 않도록 하는 로직 제거
    - clickedLinkRef 관련 로직 제거
    - 클릭 이벤트 내에서 anchor의 active/inactive 여부를 세팅하지 않고, intersection observer가 알아서 하도록 변경
    - toc 스크롤 시 너무 빠르게 이동해 어색하게 보이지 않도록 스크롤 속도 조정 (500ms -> 800ms)

--- src/components/elements/Toc/Toc.tsx
+++ src/components/elements/Toc/Toc.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useCallback } from 'react';
-import { animateScroll as scroll, Events } from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 import { NAV_BAR_HEIGHT } from '#components/NavBar';
 
 import style from './Toc.module.scss';
@@ -18,7 +18,6 @@ interface TocProps {
   children?: React.ReactElement;
 }
 export default function Toc({ children }: TocProps) {
-  const clickedLinkRef = useRef<Link>();
   const linksRef = useRef<{ h1: Link[]; h2: Link[] }>();
   const currentH2AnchorRef = useRef<HTMLAnchorElement | null>(null);
   const h1sObserverRef = useRef<IntersectionObserver | null>(null);
@@ -91,22 +90,13 @@ export default function Toc({ children }: TocProps) {
   }
 
   const moveToHeading = useCallback((link: Link) => {
-    linksRef.current?.h1.forEach(({ anchor }) =>
-      anchor.classList.remove('active', 'active-backsite'),
-    );
-    linksRef.current?.h2.forEach(({ anchor }) =>
-      anchor.classList.remove('active'),
-    );
-
-    active(link);
-
     const { heading } = link;
     scroll.scrollTo(
       window.scrollY +
         heading.getBoundingClientRect().top -
         (NAV_BAR_HEIGHT + EXTRA),
       {
-        duration: 500,
+        duration: 800,
         smooth: 'easeOutQuart',
       },
     );
@@ -120,20 +110,19 @@ export default function Toc({ children }: TocProps) {
     linksRef.current = { h1, h2 };
   }, []);
 
+  // Intersection Observer 세팅
   useEffect(() => {
     const handleHeadingObserver: (
       links: Link[],
     ) => IntersectionObserverCallback = (links) => (entries) => {
-      if (!clickedLinkRef.current) {
-        entries.forEach((entry) => {
-          const hash = entry.target.firstElementChild?.id;
-          const link = links.find(
-            ({ anchor }) => decodeURIComponent(anchor.hash) === `#${hash}`,
-          );
+      entries.forEach((entry) => {
+        const hash = entry.target.firstElementChild?.id;
+        const link = links.find(
+          ({ anchor }) => decodeURIComponent(anchor.hash) === `#${hash}`,
+        );
 
-          link && (entry.isIntersecting ? active(link) : inactive(link));
-        });
-      }
+        link && (entry.isIntersecting ? active(link) : inactive(link));
+      });
     };
 
     const links = linksRef.current;
@@ -161,11 +150,11 @@ export default function Toc({ children }: TocProps) {
     }
 
     return () => {
-      h1sObserverRef.current?.disconnect();
-      h2sObserverRef.current?.disconnect();
+      unobserve();
     };
   }, []);
 
+  // toc click event 세팅
   useEffect(() => {
     const tocElement = document.querySelector(`.${style.toc}`);
 
@@ -175,7 +164,6 @@ export default function Toc({ children }: TocProps) {
         linksRef.current?.h2.find((h2) => h2.anchor === e.target);
 
       if (link) {
-        clickedLinkRef.current = link;
         moveToHeading(link);
       }
     };
@@ -192,32 +180,7 @@ export default function Toc({ children }: TocProps) {
       );
   }, [router, moveToHeading]);
 
-  useEffect(() => {
-    const scorllHanlder = () => {
-      clickedLinkRef.current = undefined;
-    };
-
-    const links = linksRef.current;
-    if (links && links.h1.length && links.h2.length) {
-      Events.scrollEvent.register('begin', () => {
-        document.removeEventListener('scroll', scorllHanlder);
-        unobserve();
-      });
-      Events.scrollEvent.register('end', () => {
-        setTimeout(() => {
-          document.addEventListener('scroll', scorllHanlder);
-        }, 100);
-        observe();
-      });
-    }
-
-    return () => {
-      Events.scrollEvent.remove('begin');
-      Events.scrollEvent.remove('end');
-      document.removeEventListener('scroll', scorllHanlder);
-    };
-  }, []);
-
+  // hash를 가진 채로 접근 시 세팅
   useEffect(() => {
     if (window.location.hash) {
       const hash = decodeURIComponent(window.location.hash.substring(1));
@@ -227,7 +190,6 @@ export default function Toc({ children }: TocProps) {
         linksRef.current?.h2.find((h2) => h2.heading.id === hash);
 
       if (link) {
-        clickedLinkRef.current = link;
         moveToHeading(link);
       }
     }
